782d6d272fa4fa4a3d672c8f962ab91b701db298
Viktor Rudakov <53302564+vicrudakov@users.noreply.github.com>
2022-02-20 15:06:01 +0100

    Aufgabe 3.1

---
+++ functions1.R
@@ -0,0 +1,27 @@
+source("functions2.R") # Helfer-Funktionen
+
+
+# a) Eine Funktion, die verschiedene geeignete deskriptive Statistiken
+# für metrische Variablen berechnet und ausgibt
+
+
+# b) Eine Funktion, die verschiedene geeignete deskriptive Statistiken für
+# kategoriale Variablen berechnet und ausgibt
+
+
+# c) Eine Funktion, die geeignete deskriptive bivariate Statistiken für den
+# Zusammenhang zwischen zwei kategorialen Variablen berechnet ausgibt
+
+
+# d) Eine Funktion, die geeignete deskriptive bivariate Statistiken für den
+# Zusammengang zwischen einer metrischen und einer dichotomen Variablen 
+# berechnet und ausgibt
+
+
+# e) Eine Funktion, die eine mindestens ordinal skalierte Variable quantilbasiert
+# kategorisiert (z.B. in „niedrig“, „mittel“, „hoch“)
+
+
+# f) Eine Funktion, die eine geeignete Visualisierung von drei oder vier 
+# kategorialen Variablen erstellt
+
