3e9416d4e1084b2a8c7ad451b180850f564f1146
Maria Levina <sumatrana@yandex.ru>
2022-02-21 17:36:09 +0300

    3a) mit Dokumentation

--- functions1.R
+++ functions1.R
@@ -1,9 +1,48 @@
 source("functions2.R") # Helfer-Funktionen
 
 
-# a) Eine Funktion, die verschiedene geeignete deskriptive Statistiken
-# für metrische Variablen berechnet und ausgibt
+# a) 
 
+# deskr_metr - eine Funktion, die verschiedene geeignete deskriptive 
+#              Statistiken für metrische Variablen berechnet und ausgibt. 
+#              Die Statistiken sind in Output aufgezaehlt. 
+
+# Input: x - der Vektor mit Merkmalauspraegungen eines quantitatives Merkmals,
+#            der metrische Skala verwendet.
+# Output: eine bennante Liste der Statistiken, die Funktion zurueckgibt:
+#          -"Mittelwert" - eine Zahl, Mittelwert,
+#          -"Median" - eine Zahl, der Median
+#          -"Quartile" - ein 2-elementiger num. Vektor, 0.25- und 0.75-Quantile
+#          -"Extrempunkte" - ein 2-elementiger num. Vektor, Minimum und Maximum
+#          -"Quartilabstand" - eine Zahl, Quartilabstand
+#          -"MQA" - eine Zahl, mittl. quadratische Abweichung s^2,
+#               s^2 = (1/n) * sum((x[i] − mean(x))^2)
+#          -"Variationskoeffizient" - eine Zahl, Variationskoeffizient,
+#               Dispersionsmass,  v = s/mean(x)
+
+deskr_metr <- function(x){
+  
+  #Sicherheits-Check
+  stopifnot(is.vector(x), !is.na(x))
+  #Laenge des Datensatzes
+  n = length(x)
+  
+  quant <- quantile(x, c(0.25, 0.5, 0.75))
+  
+  q_abst <- quant[3] - quant[1]
+  names(q_abst) <- NULL
+  
+  mqa <- sum((x - mean(x))^2) / n
+  
+  return(list("Mittelwert" = mean(x),
+              "Median" = quant[2],
+              "Quartile" = quant[-2],
+              "Extrempunkte" = range(x),
+              "Quartilabstand" = q_abst,
+              "MQA" = mqa,
+              "Variationskoeffizient" = (sqrt(mqa) / mean(x))
+  ))
+}
 
 # b) Eine Funktion, die verschiedene geeignete deskriptive Statistiken für
 # kategoriale Variablen berechnet und ausgibt
