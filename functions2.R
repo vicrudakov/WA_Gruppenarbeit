928d67a8345a888bad0ae4704ed0e67a7e89c9b3
Yuliia Yarova <99659586+jul002@users.noreply.github.com>
2022-02-25 18:45:33 +0100

    Hilfsfunktion 3.2 c)

--- functions2.R
+++ functions2.R
@@ -1,2 +1,12 @@
 # Helfer-Funktionen
 
+
+# randvert - gibt Zeilen- oder Spaltensumme zurueck
+#
+# Input: t - eine Tabelle mit Zahlen
+# dim - 1 - Zeilen, 2 - Spalten
+#
+# Output: Zeilen- oder Spaltensumme
+randvert <- function(t, dim){
+  return(apply(t, dim, sum))
+}
