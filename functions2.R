0053f1ceb99dd451b7a8fb1447ceb7f508c48fce
Yuliia Yarova <99659586+jul002@users.noreply.github.com>
2022-02-25 18:47:23 +0100

    Hilfsfunktion 3.2 (c)
    * Hilfsfunktion 3.2 c)
    
    * Hilfsfunktion 3.2 c)

--- functions2.R
+++ functions2.R
@@ -1,24 +1,12 @@
 # Helfer-Funktionen
 
-# balkendiagramm - gibt ein Balkendiagramm von y ~ x zurueck
+
+# randvert - gibt Zeilen- oder Spaltensumme zurueck
 #
-# Input:  x - Vektor fuer x-Achse
-#         y - Vektor fuer y-Achse
-# 
-# Output: Grafik (intern als eine Liste)
-
-balkendiagramm <- function(x, y) {
-  df <- data.frame(Variablen = x, Haeufigkeit = y)
-
-  plot1 <- ggplot(df, aes(x = Variablen, y = Haeufigkeit)) + 
-    geom_bar(stat = "identity", width = 0.5,
-             fill = "steelblue", color = "black") +
-    geom_text(aes(label = Variablen), vjust = -0.5) +
-    ylim(0, 1.1 * max(df$Haeufigkeit)) +
-    theme(axis.title = element_text(size = 13),
-          axis.text = element_text(size = 13), 
-          axis.title.x = element_text(margin = margin(15, 0, 0, 0)),
-          axis.title.y = element_text(margin = margin(0, 20, 0, 0)))
-  
-  return(plot1)
+# Input: t - eine Tabelle mit Zahlen
+# dim - 1 - Zeilen, 2 - Spalten
+#
+# Output: Zeilen- oder Spaltensumme
+randvert <- function(t, dim){
+  return(apply(t, dim, sum))
 }
