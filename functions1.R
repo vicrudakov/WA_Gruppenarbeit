38236aeb481e78e2a79a19703a9e5b0eed90f760
Yuliia Yarova <99659586+jul002@users.noreply.github.com>
2022-02-25 16:27:55 +0100

    Aufgabe 3.1 c)

--- functions1.R
+++ functions1.R
@@ -1,27 +1,67 @@
 source("functions2.R") # Helfer-Funktionen
 
 
-# a) Eine Funktion, die verschiedene geeignete deskriptive Statistiken
-# für metrische Variablen berechnet und ausgibt
+# a) 
 
 
-# b) Eine Funktion, die verschiedene geeignete deskriptive Statistiken für
-# kategoriale Variablen berechnet und ausgibt
+# b) 
 
 
-# c) Eine Funktion, die geeignete deskriptive bivariate Statistiken für den
-# Zusammenhang zwischen zwei kategorialen Variablen berechnet ausgibt
+# c)
+# zshg_deskr_kat - Eine Funktion, die geeignete deskriptive bivariate Statistiken 
+#                 für den Zusammenhang zwischen zwei kategorialen Variablen 
+#                 berechnet ausgibt
+# Input: x - der Vektor mit Merkmalauspraegungen eines kategoriales Merkmals
+#        y - der Vektor mit Merkmalauspraegungen eines kategoriales Merkmals
+# Output: eine bennante Liste der briviate Statistiken, die Funktion zurueckgibt:
+#          -"Chi^2" - eine Zahl, quadratische Kontingenz,
+#               Chi^2 = sum l(sum k(hfgt[i,j]^2 / rand_x[i] * rand_y[j]))
+#          -"Pearson's Kontingenzmass" - eine Zahll, Kontingenzkoeffizient nach 
+#           Pearson
+#                Kp = sqrt(chi2 / (chi2 + n))
 
 
-# d) Eine Funktion, die geeignete deskriptive bivariate Statistiken für den
-# Zusammengang zwischen einer metrischen und einer dichotomen Variablen 
-# berechnet und ausgibt
+zshg_deskr_kat <- function(x, y){
+  #Anzahl der Antworten
+  n <- length(x)
+  
+  #Zweidimensionale Haeufigkeitsverteilung
+  hfkt <- table(x,y)
+  
+  #Anzahl moeglicher Merkmalauspraegungen
+  l <- length(unique(y))
+  k <- length(unique(x))
+  
+  #Speichern von Randverteilung von X und Y
+  rand_x <- randvert(hfkt, 1)
+  rand_y <- randvert(hfkt, 2)
+  names(rand_x) <- NULL
+  names(rand_y) <- NULL
+  
+  #Berechnen von quadratischer Kontingenz chi^2
+  chi2 <- 0
+  for(i in 1:k){
+    for(j in 1:l){
+      temp <- (hfkt[i, j]^2)
+      temp <- temp / (rand_x[i] * rand_y[j])
+      chi2 <- chi2 + temp
+    }
+  }
+  chi2 <- (chi2 - 1) * n
+  
+  #Berechnen von Pearson's Kontingenzmass
+  pearson_koef <- sqrt(chi2 / (chi2 + n))
+  
+  return(list("Chi^2" = chi2,
+              "Pearson's Kontingenzmass" = pearson_koef))
+}
 
 
-# e) Eine Funktion, die eine mindestens ordinal skalierte Variable quantilbasiert
-# kategorisiert (z.B. in „niedrig“, „mittel“, „hoch“)
+# d) 
 
 
-# f) Eine Funktion, die eine geeignete Visualisierung von drei oder vier 
-# kategorialen Variablen erstellt
+# e) 
+
+
+# f) 
 
