c37b8d70171e9ce38b3aedc8cd68d7ed8709b1dc
Yuliia Yarova <99659586+jul002@users.noreply.github.com>
2022-02-25 18:41:20 +0100

    Hilfsfunktion 3.2 c)

--- functions2.R
+++ functions2.R
@@ -1,24 +1,2 @@
 # Helfer-Funktionen
 
-# balkendiagramm - gibt ein Balkendiagramm von y ~ x zurueck
-#
-# Input:  x - Vektor fuer x-Achse
-#         y - Vektor fuer y-Achse
-# 
-# Output: Grafik (intern als eine Liste)
-
-balkendiagramm <- function(x, y) {
-  df <- data.frame(Variablen = x, Haeufigkeit = y)
-
-  plot1 <- ggplot(df, aes(x = Variablen, y = Haeufigkeit)) + 
-    geom_bar(stat = "identity", width = 0.5,
-             fill = "steelblue", color = "black") +
-    geom_text(aes(label = Variablen), vjust = -0.5) +
-    ylim(0, 1.1 * max(df$Haeufigkeit)) +
-    theme(axis.title = element_text(size = 13),
-          axis.text = element_text(size = 13), 
-          axis.title.x = element_text(margin = margin(15, 0, 0, 0)),
-          axis.title.y = element_text(margin = margin(0, 20, 0, 0)))
-  
-  return(plot1)
-}
