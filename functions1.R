45c28e5d06ad5a45ee0e375f32339d0aa90e165f
Maria Levina <sumatrana@yandex.ru>
2022-02-26 15:35:17 +0300

    Korrigierte Version von c)
    Jetzt wird die Funktion genau wie in d) eine korrigierte Pearson-Kontingenzmass berechnen und auch Phi-Koeffizient statt Chi-Quadrat ausgeben.
    
    Co-Authored-By: Yuliia Yarova <99659586+jul002@users.noreply.github.com>

--- functions1.R
+++ functions1.R
@@ -113,11 +113,11 @@ zshg_deskr_kat <- function(x, y) {
   }
   chi2 <- (chi2 - 1) * n
   
-  #Berechnen von Pearson's Kontingenzmass
   pearson_koef <- sqrt(chi2 / (chi2 + n))
+  korr_pearson_koef <- sqrt(chi2 / (chi2 + n)) * sqrt(2)
   
-  return(list("Chi^2" = chi2,
-              "Pearson's Kontingenzmass" = pearson_koef))
+  return(list(Phi = phi,
+              Korr_Pearson = korr_pearson_koef))
 }
 
 
